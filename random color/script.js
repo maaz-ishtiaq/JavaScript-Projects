b4d32dc56af722cfdb71c083cbca6fda4e9d62d9
Maaz Ishtiaq <maazishtiaq285@gmail.com>
2026-01-08 08:24:56 +0500

    Random Color Genrator

---
+++ random color/script.js
@@ -0,0 +1,21 @@
+const getcolor = () => {
+    const randomnum = Math.floor(Math.random() * 16777215)
+    const randomcode = "#" + randomnum.toString(16)
+    const body = document.body,
+    button = document.querySelector("button")
+    body.style.background = randomcode
+    button.style.backgroundColor = randomcode
+    button.style.borderColor = randomcode
+    document.querySelector("p").innerHTML = randomcode
+}
+document.getElementById("btn").addEventListener("click",
+    getcolor
+)
+
+
+
+// const dataset = ["ABCDEFGHIJKLMNOPQRSTVUWXYZ", "abcdefghijklmnopqrstvuwxyz", "1234567890" , "!@#$%^&*"]
+// const passgen = (gen) => {
+//     return gen = Math.random()
+//     return passgen(dataset)
+// }
